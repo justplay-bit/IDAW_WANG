b4ca608bee5309ebf253f68280bdc5682e9bb22e
Xuechu_WANG <xuechu.wang@etu.imt-lille-douai.fr>
2021-03-15 16:37:05 +0100

    tp2.1_new

---
+++ tp3/index.php
@@ -0,0 +1,7 @@
+<form id="style_form" action="index.php" method="GET">
+<select name="css">
+<option value="style1">style1</option>
+<option value="style2">style2</option>
+</select>
+<input type="submit" value="Appliquer" />
+</form>
